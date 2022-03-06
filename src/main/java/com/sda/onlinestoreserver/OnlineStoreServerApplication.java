c60256214ba51dea6a23ad98b09a0a083cb9e1fd
Lammutaya <sparkman.3333@gmail.com>
2022-03-06 13:47:28 +0200

    Initial commit

---
+++ src/main/java/com/sda/onlinestoreserver/OnlineStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.sda.onlinestoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/onlinestoreserver/OnlineStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.onlinestoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
